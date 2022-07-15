3b70306f5fb1780b04a8ced797e0f962daf05765
何伟文 <weiwen.he@liangyihui.net>
2022-07-15 18:34:19 +0800

    go_demo 变量学习 提交

--- 02_package/main.go
+++ 02_package/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	_ "fmt"
-	""
+	winniepooh "go_demo/02_package/icomefromalaska"
+	"go_demo/02_package/stringutil"
 )
 
 func main() {
-
+	fmt.Println(stringutil.MyName)
+	fmt.Println(stringutil.Reverse("今天是星期五"))
+	fmt.Println(winniepooh.BearName)
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
-module "go_demo"
+module go_demo
+
+go 1.0
+
+require github.com/GoesToEleven/GolangTraining v0.0.0-20181204234241-afa19f5c43f3
